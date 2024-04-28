cf59441a9546cde8f3cf699f67078094aa2315c7
João Paulo Granato Lopes <jpglopes@yahoo.com.br>
2024-04-28 20:01:33 -0300

    project created

---
+++ src/application/ContaTerminal.java
@@ -0,0 +1,23 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+	
+	private int numero;
+	private String agencia;
+	private String nomeCliente;
+	private double saldo;
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		
+		sc.close();
+
+	}
+
+}
