1f87c215c88a22aad3df309ac75d7acd5c2036e6
João Paulo Granato Lopes <jpglopes@yahoo.com.br>
2024-04-28 21:16:38 -0300

    arquivo de entrega do desafio

--- src/application/ContaTerminal.java
+++ src/application/ContaTerminal.java
@@ -1,22 +1,40 @@
 package application;
 
-import java.util.Locale;
 import java.util.Scanner;
 
 public class ContaTerminal {
-	
-	private int numero;
-	private String agencia;
-	private String nomeCliente;
-	private double saldo;
 
 	public static void main(String[] args) {
 
-		Locale.setDefault(Locale.US);
+		int numero;
+		String agencia;
+		String nomeCliente;
+		double saldo;
+
 		Scanner sc = new Scanner(System.in);
-		
-		
+
+		System.out.print("Por favor, digite o número da conta !");
+		numero = sc.nextInt();
+
+		System.out.print("Por favor, digite o número da Agencia !");
+		agencia = sc.next().toString();
+
+		System.out.print("Por favor, digite o nome do cliente !");
+		nomeCliente = sc.next();
+
+		System.out.print("Por favor, digite o valor do seu primeiro deposito !");
+		saldo = sc.nextDouble();
+
 		sc.close();
+
+		System.out.println("Olá " 
+				+ nomeCliente + ", obrigado por criar uma conta em nosso banco, " 
+				+ "sua agencia é "
+				+ agencia 
+				+ ", conta " 
+				+ numero 
+				+ " e seu saldo " 
+				+ saldo + " já está disponível para saque.");
 
 	}
 
